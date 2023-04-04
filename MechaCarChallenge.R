ebc8984c54dc86d1036ddc8e4a79962c5143a230
hheng9 <118647523+hheng9@users.noreply.github.com>
2023-04-03 21:28:13 -0700

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,14 @@ Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFa
 total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep') 
+
+t.test(Suspension_Coil$PSI, mu = 1500)
+
+# t-test for Lot1
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+
+# t-test for Lot2
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+
+# t-test for Lot3
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
